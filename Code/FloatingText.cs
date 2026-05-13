91805c3b29802cedf7ac4b458ea964113c573529
christiansteil <christian@steil.org>
2026-05-13 11:06:03 -0500

    add UI elements

--- Code/FloatingText.cs
+++
@@ -1,9 +0,0 @@
-using Sandbox;
-
-public sealed class FloatingText
-{
-	public int Id { get; set; }
-	public string Text { get; set; } = "";
-	public TimeSince Age { get; set; }
-	public Vector2 Position { get; set; }
-}
