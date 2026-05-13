860cf69fe2248977466eab83d190941ad80bbad4
christiansteil <christian@steil.org>
2026-05-12 21:41:50 -0500

    refusing ai nonsense

--- Code/SaveData.cs
+++ Code/SaveData.cs
@@ -4,19 +4,12 @@ public sealed class SaveData
 	public int Trains { get; set; }
 	public int TrainCars { get; set; }
 
-	// New save time format.
 	public long LastSaveTimeMs { get; set; }
-
-	// Old save time format, kept temporarily so old save.json files still load.
-	public long LastSaveTime { get; set; }
 
 	public long GetLastSaveTimeMs()
 	{
 		if ( LastSaveTimeMs > 0 )
 			return LastSaveTimeMs;
-
-		if ( LastSaveTime > 0 )
-			return LastSaveTime * 1000;
 
 		return 0;
 	}
